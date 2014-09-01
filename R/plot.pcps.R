410f36623d96dcfec05fc8d7fcf55e5bd9e46b60
Vanderlei Debastiani <vanderleidebastianimach@yahoo.com.br>
2014-09-01 07:35:02 -0300

    Update plot.pcps.R
    Add choices argument.

--- R/plot.pcps.R
+++ R/plot.pcps.R
@@ -1,5 +1,5 @@
-plot.pcps<-function(x,display=c("text","points"),groups,showlabel=TRUE,...){
-	sco<-scores(x,...)
+plot.pcps<-function(x,display=c("text","points"),groups,showlabel=TRUE,choices=c(1,2),...){
+	sco<-scores(x,choices=choices)
 	plot(sco$scores.sites,type="n",ylim=c(min(sco$scores.sites[,2],sco$scores.species[,2],na.rm=TRUE)-0.05, max(sco$scores.sites[,2],sco$scores.species[,2],na.rm=TRUE)+0.05),xlim=c(min(sco$scores.sites[,1],sco$scores.species[,1],na.rm=TRUE)-0.05,max(sco$scores.sites[,1],sco$sco[,1],na.rm=TRUE)+0.05),...)
 	if(display=="text"){
 		text(sco$scores.sites,labels=rownames(sco$scores.sites),...) 
